53c499ef4f508cc5392666a0fac3b89c59694803
Mathieu CHATILLON <chatillon.mathieu@gmail.com>
2020-11-16 11:43:55 +0100

    Update documentation

--- swagger/config.js
+++ swagger/config.js
@@ -2,13 +2,13 @@ const swaggerJSDoc = require('swagger-jsdoc');
 
 const swaggerDefinition = {
 	info: {
-		title: 'Pankod MoleculerJS Boilerplate', // Title of the documentation
+		title: 'AXA assessment', // Title of the documentation
 		version: '1.0.0', // Version of the app
 		description:
-			'Moleculer JS Microservice Boilerplate with Typescript, TypeORM, CLI, Service Clients, Swagger, Jest, Docker, Eslint support and everything you will ever need to deploy rock solid projects..', // short description of the app
+			'Node.js assessment using the Moleculer.js microservices framework with the Pankod boilerplate', // short description of the app
 	},
 	host: 'localhost:3000', // the host or url of the app
-	basePath: '/api', // the basepath of your endpoint
+	basePath: '/', // the basepath of your endpoint
 };
 
 // options for the swagger docs
