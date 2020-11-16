ada0dc7e1a18f04268f6fea1019aeaab48b4495b
Mathieu CHATILLON <chatillon.mathieu@gmail.com>
2020-11-16 11:42:31 +0100

    Add 401 error handler

--- src/Repositories/ErrorHelpers.ts
+++ src/Repositories/ErrorHelpers.ts
@@ -2,6 +2,13 @@
 import { Errors } from 'moleculer';
 //#endregion Global Imports
 
+export const Throw401 = (granted: boolean, message?: string) => {
+	if (!granted) {
+		throw new Errors.MoleculerError(message || '', 401, 'Unauthorized access');
+	}
+	return granted;
+};
+
 export const Throw404 = <R extends {}>(resource: R | undefined, message: string): R => {
 	if (!resource) {
 		throw new Errors.MoleculerError(message, 404, 'Not Found');
